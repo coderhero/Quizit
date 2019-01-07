03050b9c43ac06e51431f94bf5281a33d2a6e8de
yanHuang <yanhuang2013@gmail.com>
2019-01-07 10:13:48 -0500

    ERD models defined

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :quizzes
+
+  has_secure_password
+  validates :username, presence: true
+  def to_token_payload
+    {
+      sub: id,
+      username: username
+    }
+  end
+  
 end
