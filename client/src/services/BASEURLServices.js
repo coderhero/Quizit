d3eacc18a80d950473a8d33c7d8ba825abc245ac
yanHuang <yanhuang2013@gmail.com>
2019-01-14 10:33:28 -0500

    modify base_url

--- client/src/services/BASEURLServices.js
+++ client/src/services/BASEURLServices.js
@@ -1,10 +1,10 @@
 // development
 // please don't specify localhost:3000 or localhost:3001 or it will occur
 // cors issue
-// const BASE_URL = '/api';
+ const BASE_URL = '/api';
 
 // production
- const BASE_URL = 'https://nameless-earth-72847.herokuapp.com/api'
+// const BASE_URL = 'https://nameless-earth-72847.herokuapp.com/api'
 
 export {
   BASE_URL
